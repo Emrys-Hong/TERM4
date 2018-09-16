2ecb0d6c1e7a083b526cde4154708f97df687827
MandyGaoGao <40432661+MandyGaoGao@users.noreply.github.com>
2018-09-16 12:34:57 +0800

    Update MyRectangle2D.java

--- intro_to_sys/problem_set1/MyRectangle2D.java
+++ intro_to_sys/problem_set1/MyRectangle2D.java
@@ -86,3 +86,101 @@ public class MyRectangle2D {
     }
 }
 
+
+
+
+//yy ans:
+class MyRectangle2D{
+        //datafield x,y(center),height,width
+        double x=0,y=0,width=1,height=1;
+        //constructor (0,0) w 1 h1
+        MyRectangle2D(){}
+        MyRectangle2D(double x1,double y1,double width1,double height1){
+            x=x1;y=y1;width=width1;height=height1;
+        }
+        //getter,setter:x,y,h,w,
+        double getX(){return x;}
+        void setX(double x1){x=x1;}
+        
+        double getY(){return y;}
+        void setY(double y1){y=y1;}
+        
+        double getWidth(){return width;}
+        void setWidth(double width1){width=width1;}
+        
+        double getHeight(){return height;}
+        void setHeight(double height1){height=height1;}
+       
+        //method getArea getPerimeter PointInRec(double x1,double y1),RecInRec(MyRectangle2D r),overlapRec(MyRectangle2D r)
+        double getArea(){
+            return height*width;
+        }
+        double getPerimeter(){
+            return 2*(height+width);
+        }
+        boolean PointInRec(double x1,double y1){
+            double up,down,left,right;
+            up=y+height/2;
+            down=y-height/2;
+            left=x+width/2;
+            right=x-width/2;
+            if(x1>=left&&x1<=right&&y1>=down&&y1<=up){
+                return true;
+            }else{return false;}
+        }
+        boolean RecInRec(MyRectangle2D r){
+            double x1,y1,width1,height1;
+            x1=r.getX();
+            y1=r.getY();
+            width1=r.getWidth();
+            height1=r.getHeight();
+            double p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y;
+            p1x=x1+width1/2;
+            p1y=y1+height1/2;
+
+            p2x=x1-width1/2;
+            p2y=y1+height1/2;
+
+            p3x=x1+width1/2;
+            p3y=y1-height1/2;
+
+            p4x=x1-width1/2;
+            p4y=y1-height1/2;
+
+            if(PointInRec(p1x,p1y)==true&&PointInRec(p2x,p2y)==true&&PointInRec(p3x,p3y)==true&&PointInRec(p4x,p4y)==true){
+                return true;
+            }else{return false;}
+        }
+        boolean overlapRec(MyRectangle2D r){
+            boolean ans=false;
+            if(RecInRec(r)==true){
+                ans=false;}
+            else{
+
+                double x1,y1,width1,height1;
+                x1=r.getX();
+                y1=r.getY();
+                width1=r.getWidth();
+                height1=r.getHeight();
+                double p1x,p1y,p2x,p2y,p3x,p3y,p4x,p4y;
+                p1x=x1+width1/2;
+                p1y=y1+height1/2;
+
+                p2x=x1-width1/2;
+                p2y=y1+height1/2;
+
+                p3x=x1+width1/2;
+                p3y=y1-height1/2;
+
+                p4x=x1-width1/2;
+                p4y=y1-height1/2;
+                
+                
+                if(PointInRec(p1x,p1y)==true||PointInRec(p2x,p2y)==true||PointInRec(p3x,p3y)==true||PointInRec(p4x,p4y)==true){
+                    ans=true;
+                }
+
+            }
+            return ans;
+        }
+    }
