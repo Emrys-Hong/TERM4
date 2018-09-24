c8436542b98e10b0ea5da622904bd2d16985ad99
Emrys-Hong <hongpengfei.emrys@gmail.com>
2018-09-24 23:18:15 +0800

    added the first question of partB

---
+++ intro_to_sys/problem_set1/partB/PowerMod.java
@@ -0,0 +1,9 @@
+public static int powerMod(int a, int b, int m) {
+    // TODO: Implement (Problem a)
+    if (a<0 || b<0 || m<= 0) {
+        return -1;
+    }else {
+        return (int)(Math.pow(a, b)%m);
+    }
+
+}
